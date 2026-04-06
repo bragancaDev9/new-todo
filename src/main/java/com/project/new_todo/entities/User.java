6b420c55aa21d163105825a879ee39dab4db0057
Bryan Silva <bryanstos99@gmail.com>
2026-04-06 15:47:53 -0300

    Initializing authentication and authorization implementation

---
+++ src/main/java/com/project/new_todo/entities/User.java
@@ -0,0 +1,99 @@
+package com.project.new_todo.entities;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.project.new_todo.entities.enums.UserRole;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "users")
+@Entity(name = "users")
+public class User implements UserDetails {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String login;
+	private String password;
+	private UserRole role;
+	
+	public User() {}
+
+	public User(Long id, String login, String password, UserRole role) {
+		this.id = id;
+		this.login = login;
+		this.password = password;
+		this.role = role;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public UserRole getRole() {
+		return role;
+	}
+
+	public void setRole(UserRole role) {
+		this.role = role;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		if (this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+		else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+
+	@Override
+	public String getUsername() {
+		return login;
+	}
+}
---
+++ src/main/java/com/project/new_todo/entities/enums/UserRole.java
@@ -0,0 +1,16 @@
+package com.project.new_todo.entities.enums;
+
+public enum UserRole {
+	ADMIN("admin"),
+	USER("user");
+	
+	private String role;
+	
+	UserRole(String role) {
+		this.role = role;
+	}
+	
+	public String getRole() {
+		return role;
+	}
+}
